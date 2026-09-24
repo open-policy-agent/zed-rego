115cc67a12a5104cf161c8af2265373dbcc6e860
Sebastian Spaink <sebastianspaink@gmail.com>
2026-09-24 17:01:20 -0500

    feat: list package and all rules in the outline
    The outline query only matched test rules, leaving the outline panel,
    outline picker and breadcrumbs empty for regular policies. Show the
    package and every rule instead.
    
    Also tighten the test name pattern from `^test_*` to `^test_`, so rules
    like `testify` no longer get a run button.
    
    Signed-off-by: Sebastian Spaink <sebastianspaink@gmail.com>

--- languages/rego/outline.scm
+++ languages/rego/outline.scm
@@ -1,2 +1,8 @@
-(rule_head (var) @name
-  (#match? @name "^test_*")) @item
+(module
+  (package) @context
+  .
+  (ref) @name) @item
+
+(rule
+  (default)? @context
+  (rule_head (var) @name)) @item
